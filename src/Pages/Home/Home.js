f0daea08e2b755458c7f925819b31791f2a9a3b6
arfin-arif <arifmtbd@gmail.com>
2022-10-27 22:23:57 +0600

    typewriter added on home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Typewriter } from 'react-simple-typewriter'
 import './Home.css'
 
 const Home = () => {
@@ -11,6 +12,20 @@ const Home = () => {
 
                     <Col lg='6' className='d-flex justify-content-center align-items-center mt-2'>
                         <div>
+
+                            <span style={{ color: 'Black', fontFamily: "cursive", fontWeight: 'bold' }}>
+                                {/* Style will be inherited from the parent element */}
+                                <Typewriter
+                                    words={['Learn With Learn Vibe']}
+                                    loop={5}
+                                    cursor
+                                    cursorStyle='_'
+                                    typeSpeed={70}
+                                    deleteSpeed={50}
+                                    delaySpeed={1000}
+
+                                />
+                            </span>
                             <h1>Education Is A Path To Success In Life</h1>
                             <p>Education will help you to acquire whatever you want to earn.When people are educated, they can significantly contribute to their families and society in various aspects and fields, thus creating a stable and stimulating community !!!</p>
                         </div>
