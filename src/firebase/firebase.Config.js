038a6503c9ac57ee155ad5ae280eeae52d876b8a
arfin-arif <arifmtbd@gmail.com>
2022-10-27 11:43:05 +0600

    environment variable added

--- src/firebase/firebase.Config.js
+++ src/firebase/firebase.Config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyACntj86yGKyN0gis45_nx2053laxrgcqw",
-    authDomain: "learn-vibe.firebaseapp.com",
-    projectId: "learn-vibe",
-    storageBucket: "learn-vibe.appspot.com",
-    messagingSenderId: "1047778330460",
-    appId: "1:1047778330460:web:f7f7ee3c16055fd04aa399"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
